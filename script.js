b68c174a32555331fc30f74388f2376b007cfed9
EmptyBox12 <ataberktumay@gmail.com>
2021-08-25 19:42:24 +0300

    added functionality

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function Book(title, author, pages, read) {
 }
 
 Book.prototype.getName = function () {
-    return this.name;
+    return this.title;
 }
 Book.prototype.getAuthor = function () {
     return this.author;
@@ -20,18 +20,56 @@ Book.prototype.getPages = function () {
 
 }
 Book.prototype.isRead = function(){
-    if(read){
-        return true;
+    if(this.read){
+        return "Read";
     } else{
-        return false;
+        return "Not read";
     }
 }
 
+
+
 function addBookToLibrary(title, author , pages, read) {
-    library.push(new Book(title,author,pages,read));    
+    library.push(new Book(title,author,pages,read)); 
+    createCard(library);   
 }
 
-addBookToLibrary("trst", "tesst", 212, "read" );
-addBookToLibrary("trsast", "aesst", 12, "raead" );
+function createCard(library) {
+    let i = library.length-1;
 
-console.log(library);
+    const content = document.querySelector("#content");
+
+    let card = document.createElement("div");
+    card.classList.add("card");
+
+    let title = document.createElement("p");
+    title.textContent= library[i].getName();
+    title.classList.add("title");
+    card.appendChild(title);
+    
+    let author = document.createElement("p");
+    author.classList.add("author");
+    author.textContent="By "+library[i].getAuthor();
+    card.appendChild(author);
+    
+    let pages = document.createElement("p");
+    pages.classList.add("pages");
+    pages.textContent=library[i].getPages() +" pages";
+    card.appendChild(pages);
+    
+    let read = document.createElement("button");
+    read.classList.add("read")
+    read.textContent = library[i].isRead();
+    card.appendChild(read);
+    
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
+    deleteButton.textContent="Delete";
+    card.appendChild(deleteButton);
+
+    content.appendChild(card);
+
+    
+}
+addBookToLibrary("The Wandering Inn" ,"Pirateaba",1000,true);
+addBookToLibrary("Punishment and Crime" ,"Dosto",323,false);
